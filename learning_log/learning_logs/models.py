5eec72f63a14692b70592f904cf3c874bd4f3c68
sashakukhtyk <sashakukhtyk@users.noreply.github.com>
2024-08-15 10:27:46 +0300

    made migrations to the database

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    """Topic that user studying."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return string representation of Topic."""
+        return self.text
