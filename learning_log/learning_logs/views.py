4daf34fb39374e375ea8e9508970cc813a84e28e
sashakukhtyk <sashakukhtyk@users.noreply.github.com>
2024-08-17 15:21:03 +0300

    created a form for adding new topic

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm
 
 
 def index(request):
@@ -20,3 +21,20 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    """Add new topic"""
+    if request.method != 'POST':
+        # Create an empty form, no data
+        form = TopicForm()
+    else:
+        # Post method, prosed data
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("learning_logs:topics")
+
+    # Show empty form
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
