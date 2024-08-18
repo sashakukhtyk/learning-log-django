bfec2896faf36d26e2e245b085e0fe116056a673
sashakukhtyk <sashakukhtyk@users.noreply.github.com>
2024-08-18 16:32:31 +0300

    logout done

--- learning_log/users/urls.py
+++ learning_log/users/urls.py
@@ -1,9 +1,11 @@
 """Define urls for users"""
 from django.urls import path, include
+from . import views
 
 
 app_name = 'users'
 urlpatterns = [
     # Add basic URL for authentication
     path('', include('django.contrib.auth.urls')),
+    path('logout', views.user_logout, name="logout"),
 ]
--- learning_log/users/views.py
+++ learning_log/users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 
-# Create your views here.
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("/")
