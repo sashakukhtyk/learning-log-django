2f25deb8bc235cdbb862a6918f0a97b204fa9009
sashakukhtyk <sashakukhtyk@users.noreply.github.com>
2024-08-18 17:23:22 +0300

    login_required decorators added

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
@@ -8,6 +9,7 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 
+@login_required
 def topics(request):
     """Show all topics"""
     topics = Topic.objects.order_by('date_added')
@@ -15,16 +17,18 @@ def topics(request):
     return render(request, 'learning_logs/topics.html', context)
 
 
+@login_required
 def topic(request, topic_id):
-    """Show single topic"""
+    """Show a single topic"""
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
 
+@login_required
 def new_topic(request):
-    """Add new topic"""
+    """Add a new topic"""
     if request.method != 'POST':
         # Create an empty form, no data
         form = TopicForm()
@@ -40,6 +44,7 @@ def new_topic(request):
     return render(request, 'learning_logs/new_topic.html', context)
 
 
+@login_required
 def new_entry(request, topic_id):
     """Add new entry for a particular topic"""
     topic = Topic.objects.get(id=topic_id)
@@ -59,6 +64,7 @@ def new_entry(request, topic_id):
     return render(request, 'learning_logs/new_entry.html', context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     """Edit existing entry"""
     entry = Entry.objects.get(id=entry_id)
